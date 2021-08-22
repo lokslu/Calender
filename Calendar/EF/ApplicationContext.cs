16ae96dedfb6c642e43e0853274df4fb7349e2df
Александр Мельник <lokslu6@gmail.com>
2021-08-22 12:47:11 +0200

    Adds the logic to create the database

--- Calendar/EF/ApplicationContext.cs
+++ Calendar/EF/ApplicationContext.cs
@@ -12,6 +12,16 @@ namespace Calendar.EF
         public DbSet<User> Users { get; set; }
         public DbSet<Event> Events{ get; set; }
 
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+        {
+            // Database.EnsureDeleted();   // удаляем бд со старой схемой
+
+            if (!Database.CanConnect())
+            {
+                Database.EnsureCreated();   // создаем бд с новой схемой
+            }
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             
