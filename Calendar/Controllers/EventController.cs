4fc865972bc292fbc82d1a966565a38ef58e7b3a
Александр Мельник <lokslu6@gmail.com>
2021-08-22 15:48:20 +0200

    Add EventController, EventModel

---
+++ Calendar/Controllers/EventController.cs
@@ -0,0 +1,68 @@
+﻿using Calendar.EF;
+using Calendar.EF.Entities;
+using Calendar.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calendar.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class EventController : ControllerBase
+    {
+        private readonly ApplicationContext Db;
+
+        public EventController(ApplicationContext Db)
+        {
+            this.Db = Db;
+        }
+
+        [HttpPost("add")]
+        async public Task<IActionResult> AddEvent([FromBody] EventModel NewEventModel)
+        {
+            var Userid = Guid.Parse(User.Claims.FirstOrDefault(c => c.Type == "Id")?.Value);
+           
+            Event NewEvent = new Event()
+            {
+                Time = NewEventModel.Time,
+                Data = NewEventModel.Data,
+                OwnerId = Userid
+            };
+            await Db.Events.AddAsync(NewEvent);
+            await Db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpPut("update")]
+        async public Task<IActionResult> UpdateEvent([FromBody] EventModel UpdateEventModel)
+        {
+            var Userid = Guid.Parse(User.Claims.FirstOrDefault(c => c.Type == "Id")?.Value);
+         
+            Event UpdateEvent = new Event()
+            {
+                Time = UpdateEventModel.Time,
+                Data = UpdateEventModel.Data,
+                OwnerId = Userid
+            };
+            Db.Entry(UpdateEvent).State=EntityState.Modified;
+            await Db.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        async public Task<IActionResult> DeleteEvent([FromBody] Guid EventId)
+        {
+            var DeletedEvent= await Db.Events.FirstOrDefaultAsync(x => x.Id == EventId);
+            Db.Entry(DeletedEvent).State = EntityState.Deleted;
+            await Db.SaveChangesAsync();
+            return Ok();
+        }
+    }
+}
---
+++ Calendar/Models/EventModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calendar.Models
+{
+    public class EventModel
+    {
+        public DateTime Time { get; set; }
+        public string Data { get; set; }
+    }
+}
